e784f1e6f1531b2f4a10684052239ee93a6375d1
elonmusk-create <elonmusk88749@gmail.com>
2026-07-14 15:32:29 +0100

    Add Supabase config and TELEGRAM_USERNAME fallback to config.js

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 window.APP_CONFIG = {
   TELEGRAM_USERNAME: "@rocketmanmuskt02",
+  SUPABASE_URL: "https://ohfbjafmeotpiipohevj.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9oZmJqYWZtZW90cGlpcG9oZXZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQwMzQ1MTUsImV4cCI6MjA5OTYxMDUxNX0.2fWcskr6i9V0195WwpdWIS1hi5R8sxJ3ItBgdiahs0E"
 };
